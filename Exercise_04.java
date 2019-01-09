b0ecbb7cbe9d2c5addc19bcaec734a45adbcbf04
Matthew Fitzgerald <matthewfitzgerald@Matthews-MacBook-Pro.local>
2019-01-09 10:43:05 -0700

    Adding completed fundamentals labs

---
+++ Exercise_04.java
@@ -0,0 +1,29 @@
+package fundamentals.labs;
+
+/**
+ * Fundamentals Exercise 4: Volume and Surface Area
+ *
+ *      Write the necessary code to calculate the volume and surface area of a cylinder
+ *      with a radius of 3.14 and a height of 5. Print out the result.
+ *
+ */
+
+public class Exercise_04 {
+
+    public static void main(String[] args) {
+
+        // write code here
+        double r ;
+        double height;
+        double pi ;
+        double V;
+                r = 3.14;
+                height = 5;
+                pi = 3.14;
+         V = pi * r * height;
+
+        System.out.println("The volume is:" +V);
+
+
+    }
+}
